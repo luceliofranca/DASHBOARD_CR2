f090ef576f7ed45fcdde86e392117960ebe3da2f
luceliofranca <francalucelio@hotmail.com>
2024-12-02 16:53:09 -0400

    Corrige caminho com link raw do Excel

--- dashboard.py
+++ dashboard.py
@@ -19,7 +19,7 @@ except Exception as e:
     st.stop()
 
 # Título do Dashboard
-st.title("Dashboard Interativo com Streamlit")
+st.title("Dashboard CR 2 PMMT")
 
 # Exibir os Dados
 st.subheader("Dados Carregados")
